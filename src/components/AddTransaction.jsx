49f67d45511555a5d26dffbb1d7e2e583bf6005f
pkk-git <pranavkrishnakumar@gmail.com>
2023-06-03 19:58:44 +0530

    add aliceblue

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
+import {GlobalContext} from "../context/GlobalState.jsx"
 
 let AddTransaction = () => {
     let [text,setText] = React.useState("");
     let [amount, setAmount] = React.useState(0);
+    let {addTransaction} = React.useContext(GlobalContext)
+    let whenSubmit = (e) => {
+        e.preventDefault();
+        let newTransaction = {
+            id: Math.floor(Math.random()*1000000),
+            text,
+            amount: parseFloat(amount)
+        }
+        addTransaction(newTransaction)
+    } 
     let textChange = (e) => {
-        const newText = e.target.value;
+        let newText = e.target.value;
         setText(newText)
     } 
     let amountChange = (e) => {
-        const newAmount = e.target.value;
+        let newAmount = e.target.value;
         setAmount(newAmount)
     }
   return <>
     <div className='add-form'>
         <h4> Add New Transaction </h4>
-        <label className='form-label'> Transaction Name </label>
-        <input className="form-control" type="text" value={text} onChange={textChange} placeholder="Enter Transaction Name"></input>
-        <label className="form-label"> Transaction Amount</label>
-        <input className="form-control" type="number" value={amount} onChange={amountChange} placeholder="Enter Amount" />
-        <button className="btn btn-dark">Add Transaction</button>
+        <form onSubmit={whenSubmit}>
+            <label className='form-label'> Transaction Name </label>
+            <input className="form-control" type="text" value={text} onChange={textChange} placeholder="Enter Transaction Name"></input>
+            <label className="form-label"> Transaction Amount</label>
+            <input className="form-control" type="number" value={amount} onChange={amountChange} placeholder="Enter Amount" />
+            <button className="btn btn-dark">Add Transaction</button>
+        </form>
     </div>
   </>
 }
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import AppReducer from "./AppReducer"
 
 let initialState = {
-    transactions : [
-        {id:1, text:"Groceries", amount:-500},
-        {id:2, text:"Bar", amount:-5100},
-        {id:3, text:"Salary", amount:100000},
-        {id:4, text:"Dinner", amount:-1500}
-    ]
+    transactions : []
 }
 
 export let GlobalContext = React.createContext(initialState)
@@ -19,7 +14,13 @@ export let GlobalProvider = ({children}) => {
             payload : id
         })
     }
-    return (<GlobalContext.Provider value={{transactions :state.transactions, deleteTransaction}}>
+    let addTransaction = (transaction) => {
+        dispatch({
+            type :'ADD_TRANSACTION',
+            payload : transaction
+        })
+    }
+    return (<GlobalContext.Provider value={{transactions :state.transactions, deleteTransaction, addTransaction}}>
         {children}
     </GlobalContext.Provider>)
 }
